df84f0ae4d8dd90f9d8111ad7b230ab170f4ed56
Christopher Biscardi <chris@christopherbiscardi.com>
2022-02-28 07:14:21 -0800

    container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,8 @@
 import { h } from "preact";
 
-{
-  /* <h1>Write a Rust Hello, World!</h1>
-    <p>prints "hello, world" to the console using the println! macro</p>
-    <a
-      href="https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=5adfb8fa7b4abaca5ee4f8e48af8b86c"
-      class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-    >
-      Run Program!
-    </a>
-     */
-}
 export default (props) => (
-  <div>
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <h1>Run a Rust Program</h1>
     <ul
       role="list"
       class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
