52e7e5702f529f6b111e281f191c5ec7b81bea49
Christopher Biscardi <chris@christopherbiscardi.com>
2022-02-28 07:12:48 -0800

    new icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default (props) => (
           <div class="-mt-px flex divide-x divide-gray-200">
             <div class="w-0 flex-1 flex">
               <a
-                href="mailto:janecooper@example.com"
+                href="https://gist.github.com/5adfb8fa7b4abaca5ee4f8e48af8b86c"
                 class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
               >
                 {/* <!-- Heroicon name: solid/mail --> */}
@@ -53,10 +53,13 @@ export default (props) => (
                   fill="currentColor"
                   aria-hidden="true"
                 >
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
                 </svg>
-                <span class="ml-3">Email</span>
+                <span class="ml-3">View Code</span>
               </a>
             </div>
             <div class="-ml-px w-0 flex-1 flex">
@@ -72,7 +75,11 @@ export default (props) => (
                   fill="currentColor"
                   aria-hidden="true"
                 >
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+                    clipRule="evenodd"
+                  />
                 </svg>
                 <span class="ml-3">Run Program</span>
               </a>
