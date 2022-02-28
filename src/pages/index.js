927f0b81332d6e2e6c99cbfd7658ee4633f5af6f
Christopher Biscardi <chris@christopherbiscardi.com>
2022-02-28 04:51:17 -0800

    rust.new

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import { h } from "preact";
 
 export default (props) => (
   <div>
-    <h1>Deploy a Serverless Function to Netlify!</h1>
-    <p>Cross compiles to musl with Netlify's auto-build support.</p>
-    <a href="https://app.netlify.com/start/deploy?repository=https://github.com/rust-adventure/netlify-serverless-example-small">
-      <img src="https://www.netlify.com/img/deploy/button.svg" />
+    <h1>Write a Rust Hello, World!</h1>
+    <p>prints "hello, world" to the console using the println! macro</p>
+    <a
+      href="https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=5adfb8fa7b4abaca5ee4f8e48af8b86c"
+      class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+    >
+      Run Program!
     </a>
   </div>
 );
